0e87e407d15f0800b912c705f83abf850f099ac2
fhw <877172774@qq.com>
2017-04-05 15:05:45 +0800

    fhw

--- application/index/controller/ViewHtml.php
+++ application/index/controller/ViewHtml.php
@@ -3,13 +3,22 @@
 namespace app\index\controller;
 
 use think\Controller;
+use app\index\model\Data;
 
 class ViewHtml extends Controller {
     
     public function index () {
         $this->assign('name', '九州一剑知');
         
-        return $this->fetch('');
+        return $this->fetch();
+    }
+    
+    public function oneshow () {
+        $data = Data::get(['data' => 'cc是人']);
+        
+        $this->assign('data', $data);
+        
+        return $this->fetch();
     }
     
 }
