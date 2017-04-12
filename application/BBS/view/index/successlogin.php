381c868d85f0be2dc5585c113491ba62bb6fb455
fhw <877172774@qq.com>
2017-04-12 16:44:53 +0800

    fhw

--- application/BBS/view/index/successlogin.php
+++ application/BBS/view/index/successlogin.php
@@ -4,9 +4,10 @@
 <title>BBS系统</title>
 <meta name="viewport" content="initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="https://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css">  
-<script src="https://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
-<script src="https://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<?php 
+    $path = "F:/Tp5/Learn_TP5/application/BBS/view";
+    include $path . "/_head.php"; 
+?>
 </head>
 <body>
 <div class="container">
